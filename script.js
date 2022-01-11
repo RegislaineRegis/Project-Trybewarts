d666016001792356fe05bee88ede527d012d36ac
Regislaine Regis <nanafidelis@gmail.com>
2022-01-11 19:03:41 -0300

    realizaçao do requisito 3 - valindo os dados no js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-<<<<<<< HEAD
+let buttonValida = document.getElementById('buttom');
+
+buttonValida.addEventListener('click', valida);
+
+function valida(event){
+    event.preventDefault(); 
+    let inputEmail = document.getElementById('inputEmail');
+    let inputSenha = document.getElementById('inputSenha');    
+    let email = inputEmail.value;
+    let password = inputSenha.value;    
+    let emailValido = 'tryber@teste.com'
+    let senhaValida = '123456'
+    if(email === emailValido && password === senhaValida){
+        alert("Olá, Tryber!");
+    }else{
+       alert('Email ou senha inválidos!');   
+    }
+}
 
 
-=======
-// let inputEmail = document.querySelector('.inputEmail');
-// let inputSenha = document.querySelector('.inputSenha');
-// let button = document.querySelector(".buttom")
-
-// function valida(){
-//    let email = inputEmail.value;
-//    let password = inputSenha.value;
-//    let emailValido = 'tryber@teste.com'
-//    let senhaValida = '123456'
-//    if(email == emailValido && password == senhaValida){
-//        window.alert("Olá, Tryber!");
-//    }else{
-//     window.alert('Email ou senha inválidos.')   
-//    }
-// }
-
-// buttom.addEventListener('click', valida);
->>>>>>> 8105bf196d0a61f0c6494c0d93bc032313ba5576
