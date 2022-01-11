e46015702bbdea37f00dd1f1d47db2dd0ce89949
Maria Clara <claramedeiroswon@gmail.com>
2022-01-11 17:26:01 -0300

    requisito 4

---
+++ script.js
@@ -0,0 +1,17 @@
+// let inputEmail = document.querySelector('.inputEmail');
+// let inputSenha = document.querySelector('.inputSenha');
+// let button = document.querySelector(".buttom")
+
+// function valida(){
+//    let email = inputEmail.value;
+//    let password = inputSenha.value;
+//    let emailValido = 'tryber@teste.com'
+//    let senhaValida = '123456'
+//    if(email == emailValido && password == senhaValida){
+//        window.alert("Olá, Tryber!");
+//    }else{
+//     window.alert('Email ou senha inválidos.')   
+//    }
+// }
+
+// buttom.addEventListener('click', valida);
