ee9e616f7c7fbdfbc52f4a9a99641e0c0ee047eb
Regislaine <nanafidelis@gmail.com>
2022-01-13 00:30:42 -0300

    realizaçao do requisito 21 - contador

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const buttonValida = document.getElementById("buttom");
 const inputCheckbox = document.getElementById("agreement");
 const buttonEnviar = document.getElementById("submit-btn");
+const conteudoTextarea = document.getElementById("textarea");
+const paragrafo = document.getElementById("counter");
+
+function atualizaContador() {
+  let contador = conteudoTextarea.textLength;
+  paragrafo.innerHTML = conteudoTextarea.maxLength - contador;
+}
+
+conteudoTextarea.addEventListener("input", atualizaContador);
 
 function habilitaBotaoEnviar() {
   if (inputCheckbox.checked === true) {
